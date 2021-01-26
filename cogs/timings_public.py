58cbffdfe8845239419f4ed263a550a9f07df174
Frerduro <35627247+Frerduro@users.noreply.github.com>
2021-01-26 03:53:32 -0800

    Update timings_public.py

--- cogs/timings_public.py
+++ cogs/timings_public.py
@@ -90,7 +90,7 @@ class Timings(commands.Cog):
                 timing_cost = int(request["timingsMaster"]["system"]["timingcost"])
                 if timing_cost > 300:
                     embed_var.add_field(name="❌ Timingcost",
-                                        value=f"Your timingcost is {timing_cost}. Your cpu is overloaded and/or slow. Find a [better host](https://www.birdflop.com).")
+                                        value=f"Your timingcost is {timing_cost}. Your cpu is overloaded and/or slow. Find a better host.")
             except KeyError as key:
                 print("Missing: " + str(key))
 
@@ -159,10 +159,10 @@ class Timings(commands.Cog):
                 cpu = int(request["timingsMaster"]["system"]["cpu"])
                 if cpu == 1:
                     embed_var.add_field(name="❌ Threads",
-                                        value=f"You have only {cpu} thread. Find a [better host](https://www.birdflop.com).")
+                                        value=f"You have only {cpu} thread. Find a better host.")
                 if cpu == 2:
                     embed_var.add_field(name="❌ Threads",
-                                        value=f"You have only {cpu} threads. Find a [better host](https://www.birdflop.com).")
+                                        value=f"You have only {cpu} threads. Find a better host.")
             except KeyError as key:
                 print("Missing: " + str(key))
 
