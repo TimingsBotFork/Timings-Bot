277c36805c611083c25b26f6e8a5c1cf15cdf692
Frerduro <35627247+Frerduro@users.noreply.github.com>
2021-01-31 02:12:45 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,17 +8,14 @@ from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, MissingPermissions
 from dotenv import load_dotenv
 import aiohttp
-import asyncio
-import paramiko
-
+from unidecode import unidecode
 
 # import subprocess
 
-
-bot = commands.Bot(command_prefix=".", intents=discord.Intents.all(), chunk_guilds_at_startup=True,
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.default(),
                    case_insensitive=True)
-load_dotenv()
 
+load_dotenv()
 token = os.getenv('token')
 
 logging.basicConfig(filename='console.log',
@@ -29,58 +26,61 @@ logging.basicConfig(filename='console.log',
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
-# Startup Information
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game('Fork of the original timings bot'))
-    logging.info('Connected to bot: {}'.format(bot.user.name))
-    logging.info('Bot ID: {}'.format(bot.user.id))
-    logging.info('Bot fully loaded')
-    logging.info('Original creators: https://github.com/Pemigrade/botflop')
-     
+    # Marks bot as running
+    logging.info('I am running.')
 
 @bot.event
 async def on_message(message):
-    if not running_on_panel:
-        # Binflop
-        if len(message.attachments) > 0:
-            if not message.attachments[0].url.endswith(
-                    ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image')):
-                download = message.attachments[0].url
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(download, allow_redirects=True) as r:
+    # Binflop
+    if len(message.attachments) > 0:
+        if not message.attachments[0].url.endswith(
+                ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image')):
+            download = message.attachments[0].url
+            async with aiohttp.ClientSession() as session:
+                async with session.get(download, allow_redirects=True) as r:
 
-                        # r = requests.get(download, allow_redirects=True)
-                        text = await r.text()
-                        text = "\n".join(text.splitlines())
-                        if '�' not in text:  # If it's not an image/gif
-                            truncated = False
-                            if len(text) > 100000:
-                                text = text[:99999]
-                                truncated = True
-                            req = requests.post('https://bin.birdflop.com/documents', data=text)
-                            key = json.loads(req.content)['key']
-                            response = ""
-                            response = response + "https://bin.birdflop.com/" + key
-                            response = response + "\nRequested by " + message.author.mention
-                            if truncated:
-                                response = response + "\n(file was truncated because it was too long.)"
-                            embed_var = discord.Embed(title="Please use a paste service", color=0x1D83D4)
-                            embed_var.description = response
-                            await message.channel.send(embed=embed_var)
-                          
-
-@bot.event
-async def on_message(message):
-
+                    # r = requests.get(download, allow_redirects=True)
+                    text = await r.text()
+                    text = unidecode(text)
+                    text = "\n".join(text.splitlines())
+                    if '�' not in text:  # If it's not an image/gif
+                        truncated = False
+                        if len(text) > 100000:
+                            text = text[:99999]
+                            truncated = True
+                        req = requests.post('https://bin.birdflop.com/documents', data=text)
+                        key = json.loads(req.content)['key']
+                        response = ""
+                        response = response + "https://bin.birdflop.com/" + key
+                        response = response + "\nRequested by " + message.author.mention
+                        if truncated:
+                            response = response + "\n(file was truncated because it was too long.)"
+                        embed_var = discord.Embed(title="Please use a paste service", color=0x1D83D4)
+                        embed_var.description = response
+                        await message.channel.send(embed=embed_var)
     timings = bot.get_cog('Timings')
-
     await timings.analyze_timings(message)
     await bot.process_commands(message)
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Birdflop bot ping is {round(bot.latency * 1000)}ms')
+
+@bot.command(name="react", pass_context=True)
+@has_permissions(administrator=True)
+async def react(ctx, url, reaction):
+    channel = await bot.fetch_channel(int(url.split("/")[5]))
+    message = await channel.fetch_message(int(url.split("/")[6]))
+    await message.add_reaction(reaction)
+    logging.info('reacted to ' + url + ' with ' + reaction)
 
 for file_name in os.listdir('./cogs'):
     if file_name.endswith('.py'):
         bot.load_extension(f'cogs.{file_name[:-3]}')
-           
+
+
 bot.run(token)
+
+# full name: message.author.name + "#" + str(message.author.discriminator) + " (" + str(message.author.id) + ")"
