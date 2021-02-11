730cd1b18effaf5a8bb9dd957e9f67b0bc13fb3a
Frerduro <35627247+Frerduro@users.noreply.github.com>
2021-02-11 12:33:34 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,6 +31,7 @@ logging.basicConfig(filename='console.log',
                     )
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
+
 @bot.event
 async def on_ready():
     # Marks bot as running
@@ -40,16 +41,17 @@ async def on_ready():
     logging.info('Bot fully loaded')
     logging.info('Original creators: https://github.com/Pemigrade/botflop')
 
+
 @bot.event
 async def on_message(message):
     # Binflop
     if len(message.attachments) > 0 and not message.attachments[0].url.endswith(
-        ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image', '.svg')):
+            ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image', '.svg')):
         download = message.attachments[0].url
         async with aiohttp.ClientSession() as session:
             async with session.get(download, allow_redirects=True) as r:
 
-                #. r = requests.get(download, allow_redirects=True)
+                # . r = requests.get(download, allow_redirects=True)
                 text = await r.text()
                 text = unidecode(text)
                 text = "\n".join(text.splitlines())
@@ -58,32 +60,39 @@ async def on_message(message):
                     if len(text) > 100000:
                         text = text[:99999]
                         truncated = True
-                    req = requests.post('https://bin.bloom.host/documents', data=text)
+                    req = requests.post(
+                        'https://bin.bloom.host/documents', data=text)
                     key = json.loads(req.content)['key']
                     response = "https://bin.bloom.host/" + key
                     response += "\nRequested by " + message.author.mention
                     if truncated:
                         response += "\n(file was truncated because it was too long.)"
-                    embed_var = discord.Embed(title="Please use a paste service", color=0x1D83D4)
+                    embed_var = discord.Embed(
+                        title="Please use a paste service", color=0x1D83D4)
                     embed_var.description = response
                     await message.channel.send(embed=embed_var)
     timings = bot.get_cog('Timings')
     await timings.analyze_timings(message)
     await bot.process_commands(message)
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Kahti bot ping is {round(bot.latency * 1000)}ms')
 
 # Only used if the wiki library is present in the folder
+
+
 @bot.command()
 async def wiki(ctx, *args):
     if os.path.exists("wiki.py"):
         await wikilib.wiki(ctx, *args)
 
+
 @bot.command()
 async def invite(ctx):
     await ctx.send('Invite me with this link:\nhttps://discord.com/oauth2/authorize?client_id=801178754772500500&permissions=0&scope=bot')
+
 
 @bot.command(name="react", pass_context=True)
 @has_permissions(administrator=True)
